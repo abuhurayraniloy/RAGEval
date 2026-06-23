14eba475f212eb3afea6c5e4618551a634e2f200
botfigther02 <abuhurayraniloy02@gmail.com>
2026-06-24 02:44:20 +0600

    feat: implement async /complete endpoint

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from groq import AsyncGroq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("GROQ_API_KEY")
+
+client = AsyncGroq(api_key=api_key)
 
 app = FastAPI()
+
+class CompletionRequest(BaseModel):
+    prompt: str
+    max_tokens: int = 500
 
 @app.get("/")
 async def root():
     return {"Message": "Hello from the root"}
+
+@app.post("/complete")
+async def request_llm(request: CompletionRequest):
+    response = await client.chat.completions.create(
+        model = "llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": request.prompt
+            }
+        ],
+        max_tokens=request.max_tokens
+    )
+
+    return {"response": response.choices[0].message.content}
