e5531247e533067641e983d6b753503545ee9287
botfigther02 <abuhurayraniloy02@gmail.com>
2026-06-28 17:34:18 +0600

    ci: add github actions pipeline for pytest and black

--- src/database.py
+++ src/database.py
@@ -5,13 +5,8 @@ DATABASE_URL = os.getenv(
     "DATABASE_URL",
 )
 
-engine = create_async_engine(
-    DATABASE_URL,
-    echo=False
-)
+engine = create_async_engine(DATABASE_URL, echo=False)
 
 AsyncSessionLocal = async_sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False
+    bind=engine, class_=AsyncSession, expire_on_commit=False
 )
