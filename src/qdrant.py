af1ab36aff60b0ace3e7f2391bfdf3d31ee708a0
botfigther02 <abuhurayraniloy02@gmail.com>
2026-06-29 00:50:31 +0600

    changed qdrant connection setting

--- src/qdrant.py
+++ src/qdrant.py
@@ -1,7 +1,7 @@
 import os
 from qdrant_client import AsyncQdrantClient
 
-QDRANT_HOST = os.getenv("QDRANT_HOST", "qdrant")
-QDRANT_PORT = int(os.getenv("QDRANT_PORT", 6333))
-
-qdrant_client = AsyncQdrantClient(host=QDRANT_HOST, port=QDRANT_PORT)
+qdrant_client = qdrant_client = AsyncQdrantClient(
+    url=os.getenv("QDRANT_URL"),
+    api_key=os.getenv("QDRANT_API_KEY"),
+)
